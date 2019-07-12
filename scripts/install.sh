5c1d6f54d1b392ee04eb7a00494fbb81aa502f65
Andevour <georg.lauterbach@mailbox.tu-dresden.de>
2019-07-12 11:26:29 +0200

    more packages

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,9 @@
 logFile=.install_log
 writeToLog=( tee -a "${logFile}" )
 
+backupFile=../backups/$(date)
+writeToBackup=(cp -rf "${backupFile}")
+
 ece=( sudo echo -e )
 
 installFlags=(--yes --assume-yes --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages)
@@ -17,12 +20,24 @@ apti=( sudo apt-get install ${installFlags[@]})
 
 ${ece[@]} "\nInstallation has begun!" | ${writeToLog[@]}
 
-if [ ! -f "${logFile}" ]
-then
+# ? Init of log and backup directory
+
+if [ ! -f "${logFile}" ]; then
     touch "${logFile}"
 fi
 
+if [ ! -d "../backups/" ]; then
+    mkdir ../backups/
+fi
+
+if [ ! -d "${backupFile}" ]; then
+    mkdir "${backupFile}"
+fi
+
+# ? Start of actual installation
 ./removePackages.sh | ${writeToLog[@]}
 ./installPackages.sh | ${writeToLog[@]}
 
 ${ece[@]} "\nEditing GRUB has begun!" | ${writeToLog[@]}
+sudo rm -f /etc/default/grub
+sudo cp grub /etc/default/
