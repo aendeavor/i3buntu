a3c2608c3da2d9fe1cd0cb4d80fd20500a26e8af
Georg Lauterbach <georg.lauterbach@mailbox.tu-dresden.de>
2019-11-08 17:27:28 +0100

    extended script to obey streamlined design; removed Markdown PDF from extensions list
    Former-commit-id: 0591dd97368b337da909a16fcb05f033c2fcd1ed
    Former-commit-id: 17d071da1e8524cd25d767b9a4bd48827ce02307
    Former-commit-id: b81425f320ebce05b3f1785b165b063900a30e6f
    Former-commit-id: 3b434b34d5fe81bf582d009d099e82156c0a4010

--- resources/sys/vscode/extensions.sh
+++ resources/sys/vscode/extensions.sh
@@ -1,3 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-/snap/bin/code --help
+# This script serves as the installation script
+# for Visual Studio Code's extensions. It is run
+# if the user chose to do so after chosing to
+# install VS Code. A detailed overview of all
+# installed extensions can be found under
+# ./README.adoc. 
+# 
+# current version - 0.0.4
+
+# ? Preconfig
+
+if [[ -z $(which code) ]] || [[ ! -e "/snap/bin/code" ]]; then
+    exit 1
+fi
+
+CODE=$(which code)
+
+if [[ -z "${CODE}" ]]; then
+    CODE="/snap/bin/code"
+fi
+
+echo $CODE
+
+# ? Preconfig finished
+# ? Actual script begins
+
+# ? Actual script finished
+# ? Extra script begins
+
+# ? Extra script finished
+# ? Postconfiguration
