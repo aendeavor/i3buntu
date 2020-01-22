29ac856a1242bd0f861e45a75400396c86fe4926
Georg Lauterbach <georg.lauterbach@mailbox.tu-dresden.de>
2020-01-22 21:49:58 +0100

    added a VM Ware Workstation Player installation script and alias

---
+++ resources/scripts/vm.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# This script serves the installation
+# of the VM Ware Workstation Player
+# for Linux maschines.
+#
+# version 0.1.0
+
+cd /tmp
+
+# Install VM Ware Workstation Player, if
+# not already installed
+
+if [[ ! -z $(which vmplayer) ]]; then
+  echo -e "VM Ware Workstation Player already installed"
+else
+  sudo apt update &>/dev/null
+  sudo apt install build-essential linux-headers-generic &>/dev/null
+
+  wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" https://www.vmware.com/go/getplayer-linux23 &>/dev/null
+
+  chmod +x getplayer-linux &>/dev/null
+  sudo ./getplayer-linux &>/dev/null
+fi
+
+# Make VM Ware Workstation Player executable with sudo without
+# needing to enter the password of the current user
+
+if [[ ! -e "/etc/sudoers.d/vmware" ]]; then
+  sudo touch -p /etc/sudoers.d/vmware
+else
+  sudo echo '' | sudo tee /etc/sudoers.d/vmware &>/dev/null
+fi
+
+echo '# Execute VM Ware Workstation Player as sudo without password' | (sudo su -c 'EDITOR="tee -a" visudo -f /etc/sudoers.d/vmware') &>/dev/null
+
+echo -e "$(whoami) ALL=NOPASSWD:/usr/bin/vmplayer" | (sudo su -c 'EDITOR="tee -a" visudo -f /etc/sudoers.d/vmware') &>/dev/null
+
+read -p "It is recommended to restart now. Would you like to restart? [Y/n]" -r RESTART
+if [[ $RESTART =~ ^(yes|Yes|y|Y| ) ]] || [[ -z $RESTART ]]; then
+    shutdown -r now
+fi
