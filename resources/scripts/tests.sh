c7b10cf7f3050f442d8b533c53921bce474c9a12
Georg Lauterbach <georg.lauterbach@mailbox.tu-dresden.de>
2020-02-25 11:15:19 +0100

    deleted icon_theme.sh and integrated it into x_packaging.sh; updated Tela to latest version
    Former-commit-id: 53658c6b7a910e3555000a9ece23c0103b40826a
    Former-commit-id: 602272e3f46b1b711bcee74334b16eae778e0396
    Former-commit-id: 1d567b0bbd67fd81d05434eca902cafb06960d13
    Former-commit-id: ea4a49092688b07363a9f8aaca20b870abba8402

--- resources/scripts/tests.sh
+++ resources/scripts/tests.sh
@@ -21,7 +21,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Insert something to test here
 run_test() {
     echo 'Started Test'
-    exit
     echo 'Finished test'
 }
 
